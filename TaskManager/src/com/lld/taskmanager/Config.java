73a9db5b58af00e11ee01c9ccdfad0de50fdf672
rohithti <rohithti@amazon.com>
2025-02-21 23:59:58 +0530

    [TaskManager] add Config
    Add Config file

---
+++ TaskManager/src/com/lld/taskmanager/Config.java
@@ -0,0 +1,7 @@
+public class Config {
+    // Number of remainders to show if task is not marked as completed
+    public static final int MAX_REMAINDERS = 3;
+
+    // Time in ms after which remainder is shown
+    public static final int REMAINDER_TIME_MIN = 10;
+}
